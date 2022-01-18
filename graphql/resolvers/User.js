d726e92679763287934c76b3017e7bd7e9cc9f4e
Dhruv <dhruvsakariya2304@gmail.com>
2022-01-18 14:35:22 +0530

    login and register Input

--- graphql/resolvers/User.js
+++ graphql/resolvers/User.js
@@ -3,10 +3,53 @@ const bcryptjs = require("bcryptjs");
 const jsonwebtoken = require("jsonwebtoken");
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
-const { validateRegisterInput } = require("../../utils/validators");
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../utils/validators");
+
+function generateToken(user) {
+  return jsonwebtoken.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+}
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if(!valid){
+       throw new UserInputError('Errors',{errors})
+      }
+      
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+      const match = await bcryptjs.compare(password, user.password);
+
+      if (!match) {
+        errors.general = "Wrong crendetials";
+        throw new UserInputError("Wrong crendetials", { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
     async register(
       _,
       { registerInput: { username, email, password, confirmPassword } }
@@ -50,15 +93,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jsonwebtoken.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res);
       return {
         ...res._doc,
         id: res._id,
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -26,7 +26,8 @@ const typeDefs = gql`
     getPosts: [Post]
   }
   type Mutation {
-    register(registerInput: RegisterInput): User
+    register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
   }
 `;
 
