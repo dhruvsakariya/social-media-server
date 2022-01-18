318ca758febcb9b45dd10bc8767cbf5eeed33b00
Dhruv <dhruvsakariya2304@gmail.com>
2022-01-18 10:18:10 +0530

    MongoDb Connected successfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require("apollo-server");
 const mongoose = require("mongoose");
+const { MONGODB } = require("./config");
 
 const typeDefs = gql`
   type Book {
@@ -27,8 +28,12 @@ const books = [
     author: "Paul Auster",
   },
 ];
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
+  console.log("mongo db connected");
+  return server.listen({ port: 5000 }).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
 });
