95a6b314cf0afe9ce5d47bf21a2e595c82bd5952
Dhruv <dhruvsakariya2304@gmail.com>
2022-01-26 10:26:21 +0530

    config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://dhruvsakariya:dhruvsakariya@cluster0.zjsru.mongodb.net/MERNG?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
